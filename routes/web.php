0fd0ca7cd6ce9ade764a621042b847e962724a16
owino600 <owinodaniel600@gmail.com>
2026-07-07 11:11:57 +0000

    on track

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,13 @@ use GuestConnect\Controllers\UniFiController;
 use GuestConnect\Controllers\PortalController;
 use GuestConnect\Controllers\AuthController;
 
-$router->get('/', [HomeController::class, 'index']);
+// Make the portal the home page
+$router->get('/', [PortalController::class, 'login']);
 
 $router->get('/test/guest', [TestController::class, 'guest']);
-
 $router->get('/test/unifi', [UniFiController::class, 'test']);
+
 $router->get('/login', [PortalController::class, 'login']);
+
+// We'll change this to POST later
 $router->get('/authorize', [AuthController::class, 'authorize']);
